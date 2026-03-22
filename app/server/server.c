42c8267439eb1d6d51904d5037ecb7665976ba74
lobster <polyak.daavid@gmail.com>
2026-03-22 16:21:22 +0100

    Update server code: accept cmd arguments

--- app/server/server.c
+++ app/server/server.c
@@ -6,7 +6,7 @@ void print_help(const char *program_name) {
    printf("Usage: %s --ip <IP> --port <PORT> [OPTIONS]...\n", program_name);
    printf("\n");
    printf("Arguments:\n");
-   printf("\t<IP> - IPv4 address owned by the host\n");
+   printf("\t<IP> - IPv4 address\n");
    printf("\t<PORT> - port number [1-65535]\n");
    printf("\n");
    printf("Options:\n");
